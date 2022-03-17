4528e3ca9f6446ba3216aaa15fbcf784b253cc0c
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2022-03-17 11:18:51 -0400

    generalize: this is the language of rings

--- src/Sigma11Ring.hs
+++ src/Sigma11Ring.hs
@@ -1,20 +1,20 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 
--- Sigma_1^1 formulas over the language {0,1,-1,+,*} of fields.
+-- Sigma_1^1 formulas over the language {0,1,-1,+,*} of rings.
 -- A Sigma^1_1 formula is a first order logic formula surrounded
 -- by zero or more second-order existential quantifiers quantifying
--- over unary functions on the field domain.
-module Sigma11Field
+-- over unary functions on the ring domain.
+module Sigma11Ring
   ( SecondOrderFormula (SecondOrderFormula)
   , SecondOrderQuantifier (ExistsFunction)
   , FirstOrderFormula (Exists, ForAll, And, Or, Not, Top, Bottom, Equals)
   , Term (Apply, Plus, Times, Constant, Variable)
   , FunctionArity (FunctionArity)
   , FunctionName (FunctionName)
-  , FieldElementName (FieldElementName)
+  , RingElementName (RingElementName)
   , DeBruijnIndex (DeBruijnIndex)
-  , FieldElement (FieldElement)
+  , RingElement (RingElement)
   , UpperBound (UpperBound)
   ) where
 
@@ -43,8 +43,8 @@ data Term =
     Apply FunctionName [Term]
   | Plus Term Term
   | Times Term Term
-  | Constant FieldElement
-  | Variable FieldElementName
+  | Constant RingElement
+  | Variable RingElementName
 
 
 newtype FunctionArity = FunctionArity Integer
@@ -59,19 +59,20 @@ newtype FunctionName = FunctionName DeBruijnIndex
 -- 1 refers to the variable quantified over by the innermost
 -- first-order quantifier.
 -- 2 refers to the second-innermost, and so on.
-newtype FieldElementName = FieldElementName DeBruijnIndex
+newtype RingElementName = RingElementName DeBruijnIndex
 
 
 -- A positive integer.
 newtype DeBruijnIndex = DeBruijnIndex Integer
 
 
--- An element of some unspecified field. This is short-hand for
--- 0, 1, -1, or some sum thereof. Our intended models are finite fields,
--- and for these fields, we include a constant symbol for each
+-- An element of some unspecified rings. This is short-hand for
+-- 0, 1, -1, or some sum thereof. Our intended models are 
+-- the integers mod n,
+-- and for these rings, we include a constant symbol for each
 -- element, and in fact, infinitely many distinct constant
 -- symbols for each element.
-newtype FieldElement = FieldElement Integer
+newtype RingElement = RingElement Integer
 
 
 newtype UpperBound = UpperBound Term
